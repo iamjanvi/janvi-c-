e62dfe0c6fde28184055862b3d0c47365d9e81a2
iamjanvi <144511672+iamjanvi@users.noreply.github.com>
2023-10-04 09:20:23 +0530

    Add files via upload

---
+++ binary +.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+ class mul
+ {
+ int a,b;
+ public: 
+ mul()
+ {}
+ mul(int x, int y)
+ {
+ a=x;
+ b=y;
+ }
+ void show()
+ {
+ cout<<a<<b<<endl;
+ }
+ mul operator +(mul);
+ };
+ mul mul ::operator +(mul t)
+ {
+ mul m;
+ m.a=a+t.a;
+ m.b=b+t.b;
+ return m;
+ }
+int main()
+{
+    mul m1(4,5),m2(5,3),m3;
+    m3=m1+m2;
+    m1.show();
+    m2.show();
+    m3.show();
+    return 0;
+}
---
+++ binary minus.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+ class number
+ {
+ int a,b;
+ public: 
+ void  get()
+ {
+ cout<<"enter two number";
+ cin>>a>>b;
+ }
+ void show()
+ {
+ cout"a ="<<a;
+ cout"b ="<<b;
+ }
+ void  operator +()
+ {
+ -a=a;
+ -b=b;
+ }
+ 
+ };
+ 
+int main()
+{
+    number n1;
+    n1.get();
+    n1.show();
+    +n1;
+    n1.show();
+    return 0;
+}
